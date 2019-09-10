2b4c9ce55b07bc173e8158d22ed49cc5eaf90551
Gary Chen <garychen.whc@gmail.com>
2019-09-10 16:10:25 +0000

    word count completed with jquery implemented with this method

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,14 @@
 $(document).ready(function() {
-  const element = document.querySelector("textarea");
-  element.addEventListener("keydown", function(e) {
-    // console.log(e);
-    console.log(this.value.length);
-    console.log(140 - this.value.length);
-    const remaining = 140 - this.value.length;
-    // const count = document.querySelector('.counter');
-    // count.textContent = 140 - this.value.length;
-    $(".counter").text(140 - this.value.length);
-    // if ('.counter' === this.silbings('.counter')) {
-    //   console.log(this);
-    //   this.textContent = remaining;
-    // }
+  let maxWord = 140;
+  $("textarea").keydown(function() {
+    let remaining = maxWord - $(this).context.value.length;
+    $(this)
+      .parent()
+      .find("span")
+      .html(remaining);
 
-    if (remaining < 0) {
-      $(".counter").addClass('countLimit');
-    } else if (remaining >= 0) {
-      $('.counter').removeClass('countLimit');
-    }
+    remaining < 0
+      ? $(".counter").addClass("countLimit")
+      : $(".counter").removeClass("countLimit");
   });
 });
