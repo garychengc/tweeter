549da5f67d6f7a853a68e39578f795c8cc9db785
Gary Chen <garychen.whc@gmail.com>
2019-09-11 18:22:49 +0000

    tweet order updated, newest one on the top

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,11 +6,10 @@
 
 const renderTweets = async function(tweets) {
   try {
-    const appendData = []; 
+    let appendData = []; 
     await tweets.forEach(tweet => {
-      appendData.push(createTweetElement(tweet));
+      appendData.unshift(createTweetElement(tweet));
     });
-
     $("#tweetContainer").append(appendData.join(''));
   } catch {error => console.log(error)};
   
@@ -24,8 +23,12 @@ const createTweetElement = tweet => {
   if (result >= 365) {
     result = Math.floor(result / 365);
     result = `${result} years ago`;
-  } else {
+  } else if (result >= 1) {
+    result = Math.floor(result);
     result = `${result} days ago`;
+  } else {
+    result = Math.floor(result * 24)
+    result = `${result} hours ago`;
   }
 
   return `
@@ -69,33 +72,6 @@ const loadTweets = () => {
   });
 };
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// const tweetData = [
-//   {
-//     user: {
-//       name: "Newton",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@SirIsaac"
-//     },
-//     content: {
-//       text:
-//         "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     created_at: 1461116232227
-//   },
-//   {
-//     user: {
-//       name: "Descartes",
-//       avatars: "https://i.imgur.com/nlhLi3I.png",
-//       handle: "@rd"
-//     },
-//     content: {
-//       text: "Je pense , donc je suis"
-//     },
-//     created_at: 1461113959088
-//   }
-// ];
-
 //Ajax request
 //call the tweetdata rendering function
 $(document).ready(() => {
@@ -112,7 +88,7 @@ $(document).ready(() => {
       alert("You haven't entered anything yet");
     } else {
       let str = $("form").serialize();
-      
+
       this.reset();
       $(this)
         .find("span")
@@ -122,6 +98,7 @@ $(document).ready(() => {
         type: "POST",
         data: str,
         success: function() {
+          $('#tweetContainer').empty();
           loadTweets();
         }
       });
